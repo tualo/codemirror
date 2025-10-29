f223bf6d0f955828f322863d85db6fb8cc9c6b25
Thomas Hoffmann <t.hoffmann.1979@googlemail.com>
2025-10-29 22:39:05 +0100

    Version bump to 1.0.2

--- src/Routes/PublicRoute.php
+++ src/Routes/PublicRoute.php
@@ -1,16 +1,20 @@
 <?php
+
 namespace Tualo\Office\Codemirror\Routes;
+
 use Tualo\Office\Basic\TualoApplication as App;
 use Tualo\Office\Basic\Route as BasicRoute;
 use Tualo\Office\Basic\IRoute;
 
 
-class PublicRoute implements IRoute{
-    public static function register(){
-        BasicRoute::add('/codemirror/(?P<file>[\/.\w\d\-\_]+)'.'.js',function($matches){
-            if (file_exists( dirname(__DIR__,2).'/lib/'.$matches['file'].'.js') ){
-                App::etagFile( dirname(__DIR__,2).'/lib/'.$matches['file'].'.js' , true);
+class PublicRoute extends \Tualo\Office\Basic\RouteWrapper
+{
+    public static function register()
+    {
+        BasicRoute::add('/codemirror/(?P<file>[\/.\w\d\-\_]+)' . '.js', function ($matches) {
+            if (file_exists(dirname(__DIR__, 2) . '/lib/' . $matches['file'] . '.js')) {
+                App::etagFile(dirname(__DIR__, 2) . '/lib/' . $matches['file'] . '.js', true);
             }
-        },['get'],false);
+        }, ['get'], false);
     }
 }
